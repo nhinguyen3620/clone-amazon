379239efc94e0ee4158c3c1a789f016d0b65cc06
nhinguyen3620 <76855747+nhinguyen3620@users.noreply.github.com>
2021-09-08 13:46:51 -0400

    Update App.js
    update app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import React from "react";
+import React, {useEffect} from "react";
 import './App.css';
 import { BrowserRouter as Router, Switch, Route} from "react-router-dom"
 import Header from "./Header";
 import Home from "./Home";
 import Checkout from "./Checkout";
+import Payment from "./Payment";
+import Login from "./Login";
+import Orders from "./Orders";
+import { auth } from "./firebase";
+import { useStateValue } from "./StateProvider";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+const promise = loadStripe('pk_test_51JKr47I3v9bWreEQwVC7NM68D4ZWL8rsNPEYUFpCjZYmx8guDwFfKH3oDc5Nz1Jmdwnd5dsdrvHjrrjYYVE5FgpK00TEv4kf5B');
+
 
 function App() {
+  const [{}, dispatch] = useStateValue();
+  useEffect(() => {
+    auth.onAuthStateChanged(authUser => {
+      console.log('THE USER IS >>> ', authUser);
+
+      if (authUser) {
+        dispatch({
+          type: 'SET_USER',
+          user: authUser
+        })
+      } 
+      else {
+        dispatch({
+          type: 'SET_USER',
+          user: null
+        })
+      } 
+    })
+  }, [])
+
   return (
     <Router>
       <div className="app">
         <Switch> 
+        <Route path="/orders">
+            <Header />
+            <Orders />
+          </Route>
           <Route path="/checkout">
-          <Header/>
-            
+            <Header />
             <Checkout />
           </Route>
+          <Route path="/payment">
+            <Header />
+            <Elements stripe={promise}>
+              <Payment />
+            </Elements>
+          </Route>
           <Route path="/login">
-            <h1>Login page</h1>
+            <Login />
           </Route>
           <Route path="/">  
-            <Header/>
+            <Header />
             <Home/>
           </Route>
         </Switch>
