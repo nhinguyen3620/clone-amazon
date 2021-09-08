b248939eaf378a3651ad88755d0c40d7c398e9c0
nhinguyen3620 <76855747+nhinguyen3620@users.noreply.github.com>
2021-09-08 13:52:29 -0400

    Create Orders.js

---
+++ src/Orders.js
@@ -0,0 +1,41 @@
+import React, {useEffect, useState} from 'react';
+import './Orders.css'
+import {db} from './firebase';
+import { useStateValue } from "./StateProvider";
+import Order from './Order';
+
+function Orders() {
+    const [{basket, user}, dispatch] = useStateValue();
+    const [orders, setOrders] = useState([]);
+
+    useEffect(() => {
+        if (user) {
+            db
+            .collection('users')
+            .doc(user?.uid)
+            .collection('orders')
+            .orderBy('created', 'desc')
+            .onSnapshot(snapshot => (
+                setOrders(snapshot.docs.map(doc => ({
+                    id: doc.id,
+                    data: doc.data()
+                })))
+            ))
+        }
+        else {
+            setOrders([])
+        }
+        
+    }, [user])
+
+    return (
+        <div className='orders'>
+            <div className='orders__order'></div>
+            {orders?.map(order => (
+                <Order order={order} />
+            ))}
+        </div>
+    )
+}
+
+export default Orders
